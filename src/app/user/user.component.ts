057d451745b014d8a2f00b0fb811ffa20b415052
Denis Korniychuk <new79den@gmail.com>
2017-01-21 02:31:49 +0300

    Урок 73: Extracting Route Parameters

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,20 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import {Router} from "@angular/router";
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {Router, ActivatedRoute} from "@angular/router";
+import {Subscribable} from "rxjs/Observable";
 
 @Component({
   selector: 'roo-user',
   template: `<p>
   user works!
   </p>
-  <a [routerLink]="['/user']">User</a>
-  <!--<a [routerLink]="['../']">Home</a>-->
+
   <br>
   <button (click)="onNavigate()">Go home</button>
+  <hr>
+  {{id}}
 `,
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
+  private subscription: any;
+  id: string;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private activatedRouter: ActivatedRoute) {
+   this.subscription = activatedRouter.params.subscribe(
+      (param: any) => this.id = param['id']
+    );
+  }
 
   onNavigate(){
     this.router.navigate(['/']);
@@ -22,5 +30,7 @@ export class UserComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
 }
